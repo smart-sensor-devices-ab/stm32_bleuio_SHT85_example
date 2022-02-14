ceeb19c6655532b0ca4f004d70c81170dced69a9
Shuhad <shuhadz@hotmail.com>
2022-02-14 11:25:40 +0100

    board to sensor id

--- web script/index.js
+++ web script/index.js
@@ -9,17 +9,17 @@ document.getElementById('getdata').addEventListener('click', function(){
   document.getElementById('loader').innerHTML = 'Loading'
   //set the BleuIO dongle into dual role
     my_dongle.at_dual().then(()=>{
-      // board id of the device that we are trying to get data from
-      let boardID='05084FA3'
+      // sensor id of the device that we are trying to get data from
+      let sensorID='05084FA3'
 
-      //look for advertised data of with the board id
-        my_dongle.at_findscandata(boardID,4).then(x=>{        
+      //look for advertised data of with the sensor id
+        my_dongle.at_findscandata(sensorID,4).then(x=>{        
 
           //split the advertised data from the respnse
           let advdata= x[x.length-1].split(" ").pop()
 
           //trim the advertised string to only get sensor response
-          const result = advdata.split(boardID).slice(1).join(boardID) 
+          const result = advdata.split(sensorID).slice(1).join(sensorID) 
 
           //get temperature and humidity value
           let temp = result.substring(0, 4);
